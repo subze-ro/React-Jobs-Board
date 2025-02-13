1fe625b68ae616c335da9877af58d6aec672c76e
Alex <alexnjoroge102@gmail.com>
2025-02-13 12:24:55 +0300

    useState and useEffect in fetch job listings from a json-server backend API instead of loading jobs from a local file and add a spinner motion in Joblistings

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,21 +1,48 @@
 import React from 'react'
-import jobs from '../jobs.json'
+import {useState, useEffect} from 'react'
 import JobListing from './JobListing'
+import Spinner  from './Spinner'
 
-const JobListings = () => {
-  const recentJobs =jobs.slice(0, 3);
+const JobListings = ({isHome = false}) => {
+  const [jobs, setJobs] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+
+  useEffect( () => {
+    const fetchJobs = async () => {
+      try {
+        const res = await fetch('http://localhost:8000/jobs');
+      const data =  await  res.json();
+      setJobs(data);
+
+      } catch (error) {
+        console.log('Error fetching Data', error);
+      } finally  {
+        setLoading(false);
+      }
+      
+    }
+
+    fetchJobs();
+  }, [])
  
    return (    
      <section className="bg-blue-50 px-4 py-10">
      <div className="container-xl lg:container m-auto">
        <h2 className="text-3xl font-bold text-cyan-700 mb-6 text-center">
-         Browse Jobs
+        {isHome ? 'Recent Jobs' : 'Browse Jobs'} 
        </h2>
        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-        {recentJobs.map((job) => (
+        {loading ? (
+          <Spinner loading={loading}/>
+        ) : (
+        <>          
+          {jobs.map((job) => (
             <JobListing key= {job.id} job= {job}/>
 
         ))}
+        </>)}
+        
                  
         
        </div>
