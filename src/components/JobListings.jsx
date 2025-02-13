90e99e51c10ceec581199bafaab1469b33a7bcb3
Alex <alexnjoroge102@gmail.com>
2025-02-13 16:45:19 +0300

    adding three jobs on home page and all jobs on jobs page

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -32,20 +32,20 @@ const JobListings = ({isHome = false}) => {
        <h2 className="text-3xl font-bold text-cyan-700 mb-6 text-center">
         {isHome ? 'Recent Jobs' : 'Browse Jobs'} 
        </h2>
-       <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+      
         {loading ? (
           <Spinner loading={loading}/>
         ) : (
-        <>          
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">     
           {jobs.map((job) => (
             <JobListing key= {job.id} job= {job}/>
 
-        ))}
-        </>)}
+              ))}
+        </div>)}
         
                  
         
-       </div>
+       
      </div>
    </section>
 
