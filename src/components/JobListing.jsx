89717c52bac32d22c829a0b88f321b4a4b4cf5f1
Alex <alexnjoroge102@gmail.com>
2025-02-14 12:30:05 +0300

    adding an add job page and then adding state to each field for react to update in sync

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -40,7 +40,7 @@ const JobListing = ({job}) => {
                   {job.location}
                 </div>
                 <Link
-                  to={`/job/${job.id}`}
+                  to={`/jobs/${job.id}`}
                   className="h-[36px] bg-cyan-700 hover:bg-cyan-700 text-white px-4 py-2 rounded-lg text-center text-sm"
                 >
                 Read More
